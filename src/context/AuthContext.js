cd4bdbff5bd59bec197ea610b95fd76f6f0fcc4a
Brian Butterly <butterlybrian@gmail.com>
2021-04-12 14:10:29 -0500

    working on auth

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with email and password.
+    //if we sign up, modify state, say we are authenticated.
+    //if signing up fails, need to show an error message somewhere.
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    //try to sign in.
+    //handle success by updating state.
+    //handle failure by showing error message.
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    //somehow sign out.
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
